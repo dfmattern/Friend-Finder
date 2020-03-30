7b7877e9df4f5d5727d4db8a9c33ec94d8aad8ff
Dane <56905214+dfmattern@users.noreply.github.com>
2020-03-29 23:57:42 -0600

    coded friend.js

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friendMatch = [
+  {
+    name: "Claire Standish",
+    photo:
+      "https://www.wamc.org/sites/wamc/files/styles/x_large/public/201602/molly-ringwald-1.jpg ",
+    scores: ["2", "3", "2", "1", "2", "2", "3", "1", "2,", "3"]
+  },
+  {
+    name: "John Bender",
+    photo:
+      "https://images-na.ssl-images-amazon.com/images/I/51VXVlUPssL._AC_.jpg ",
+    scores: ["3", "2", "2", "3", "3", "2", "2", "3", "1", "1"]
+  },
+  {
+    name: " Allison Reynolds",
+    photo:
+      "https://static.parade.com/wp-content/uploads/2015/03/ally-sheedy-breakfast-club.jpg ",
+    scores: ["1", "1", "1", "1", "1", "3", "3", "3", "2", "1"]
+  },
+  {
+    name: "Andrew Clark",
+    photo:
+      "https://cdn.citybeat.com/files/base/scomm/cb/image/2016/06/640w/the_breakfast_club_334.jpg",
+    scores: ["3", "3", "3", "3", "1", "2", "1", "2", "3", "1"]
+  },
+  {
+    name: "Brian Johnson",
+    photo: "https://www4.pictures.zimbio.com/mp/xuZxlHTfK1Kx.jpg",
+    scores: ["3", "2", "2", "3", "1", "2", "1", "3", "1", "1"]
+  }
+];
+
+module.exports = friendMatch;
